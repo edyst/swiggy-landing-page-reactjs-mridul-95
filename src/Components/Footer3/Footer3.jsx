59be942899f4daf0f32fd50018fc11b3db2e3a43
Mridul Ranjan <mridul0011.cse19@chitkara.edu.in>
2021-12-16 14:35:53 +0530

    changes

---
+++ src/Components/Footer3/Footer3.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Footer3.css'
+export default function Footer3() {
+    return (
+        <div className="Footer3">
+            <img src="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_284/Logo_f5xzza"/>
+            <h4>2021 Swiggy</h4>
+            <div className="icons">
+                <i className="fab fa-facebook"></i>
+                <i className="fab fa-facebook"></i>
+                <i className="fab fa-facebook"></i>
+                <i className="fab fa-facebook"></i>
+            </div>
+        </div>
+    )
+}
